aaa538aac821a8b971e55e424cc041012a135c7a
Chan <76525822+duong-nguy@users.noreply.github.com>
2024-09-19 16:24:46 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 import os
 import random
 
-import timm
 from torch.utils.data import DataLoader, Dataset, random_split
 import lightning as L
-from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.tuner import Tuner
 
 from model import LitAODregressor 
@@ -23,8 +21,6 @@ def train(args):
     if args.sainity_check:
         trainer = L.Trainer(max_epochs=1,limit_train_batches=5, limit_val_batches=2)
     trainer = L.Trainer(max_epochs=args.max_epochs)
-    tuner = Tuner(trainer)
-    lr_finder = tuner.lr_find(model,train_loader,valid_loader)
     trainer.fit(model, train_loader,valid_loader)
 
 
